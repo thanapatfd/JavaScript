164d1d25eae84fb4b3b9c0a254897662229d18aa
thanapatfd <68782076+thanapatfd@users.noreply.github.com>
2022-12-28 09:19:16 +0700

    Create var_let_const.js

---
+++ Lecture-2/var_let_const.js
@@ -0,0 +1,17 @@
+var hello = "Hello";
+
+console.log(hello);
+hello = "Hello World";
+console.log(hello);
+if(true){
+    let world = "Hello World";
+    console.log(world);
+}
+
+// console.log(world);
+
+const fixal = "Fix Value";
+console.log(fixal);
+
+// fixal = "test assignment value of const";
+console.log(fixal);
