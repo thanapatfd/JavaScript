5f7379dd19d026f9a01a3d88cd20d5e9979ae9a4
thanapatfd <68782076+thanapatfd@users.noreply.github.com>
2022-12-28 11:07:57 +0700

    Create mathobj.js

---
+++ Lecture-2/mathobj.js
@@ -0,0 +1,11 @@
+console.log(Math.floor(5.05));
+//Output : 5
+
+console.log(Math.floor(7.004));
+//Output : 8
+
+console.log(Math.random());
+//Output : a number from 0 to < 1
+
+console.log(Math.min(-2,-3,-1));
+//Output : -3
