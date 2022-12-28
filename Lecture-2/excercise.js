08f9b7e748b65c876fcdac98a9f7e1e70c569efe
thanapatfd <68782076+thanapatfd@users.noreply.github.com>
2022-12-28 11:07:53 +0700

    Create excercise.js

---
+++ Lecture-2/excercise.js
@@ -0,0 +1,9 @@
+function findPerimeterRectangle(length, width) {
+    let num1 = length;
+    let num2 = width;
+    let result = (length*2) + (width*2);
+    return result;
+
+}
+
+console.log(findPerimeterRectangle(6,7));
