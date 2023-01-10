5058e48ce0de25b2573dfaa40d91beed17f17f69
thanapatfd <68782076+thanapatfd@users.noreply.github.com>
2023-01-11 01:23:30 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,30 @@
-console.log("Hello World")
-console.log("Hello World")
-console.log("I love u")
+console.log(limesToCut(42, ['small', 'small', 'large', 'medium', 'small']));
+function limesToCut(wedgesNeeded, limes) {
+  let i = 0; 
+  let limes_count = 0;
+  while(true){
+    if(!(wedgesNeeded <= 0)){
+      if(limes[i] === 'small') {
+        wedgesNeeded -= 6;
+        limes_count += 1;
+        i++;
+      }
+      else if(limes[i] === 'medium'){
+        wedgesNeeded -= 8;
+        limes_count += 1;
+        i++;
+      }
+      else if(limes[i] === 'large'){
+        wedgesNeeded -= 10;
+        limes_count += 1;
+        i++;
+      }
+    }
+    else{
+      break;
+    }
+   
+  }
+  return limes_count;
+  
+}
