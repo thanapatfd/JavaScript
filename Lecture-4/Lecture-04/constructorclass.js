44f47dcd76c4d0944ed43d3a17972c7eed36ddfe
thanapatfd <68782076+thanapatfd@users.noreply.github.com>
2023-01-11 01:25:13 +0700

    Create constructorclass.js

---
+++ Lecture-4/Lecture-04/constructorclass.js
@@ -0,0 +1,10 @@
+class Song {
+    constructor(title, artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+}
+
+const mySong = new Song("Bohemain Rhapsody", "Queen");
+console.log(mySong.title);
+console.log(mySong.artist);
