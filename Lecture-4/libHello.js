859ef0665ae6b5cff013e0e4cd368d8c913aedc2
thanapatfd <68782076+thanapatfd@users.noreply.github.com>
2023-01-11 09:16:53 +0700

    Create libHello.js

---
+++ Lecture-4/libHello.js
@@ -0,0 +1,18 @@
+// Libhello.js
+const person = {
+    name: 'Shinnawat',
+    weight: 76
+}
+
+function sayHello() {
+    console.log("Hello World!");
+}
+
+function cube(x) {
+    return x * x * x;
+}
+
+const add = (a, b) => a + b;
+const status = true;
+
+module.exports = { person, sayHello, cube, add, status }
