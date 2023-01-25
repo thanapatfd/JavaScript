fcc27088ee253dc1de26b3fe118870bc58659ee7
thanapatfd <68782076+thanapatfd@users.noreply.github.com>
2023-01-25 15:11:18 +0700

    Create javascriptPromise.js

---
+++ Lecture-5/javascriptPromise.js
@@ -0,0 +1,12 @@
+const promise = new Promise((resolve, reject) => {
+    const res = true;
+    if(res){
+        resolve("Resolved");
+    }else{
+        // reject(Error("Fatal Error"));
+        reject('Error');
+    }
+});
+
+promise.then((res) => console.log(res),(err) => console.log(err));
+// promise.then((res) => console.log(res),(err) => alert(err));
