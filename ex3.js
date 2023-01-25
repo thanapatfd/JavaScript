c75b3b06f9c1aeb29632f608b4c535e2fc316846
thanapatfd <68782076+thanapatfd@users.noreply.github.com>
2023-01-25 11:36:17 +0700

    Create ex3.js

---
+++ ex3.js
@@ -0,0 +1,11 @@
+function capToFront(str) {
+    let strRe = [];
+    for(let i = 0; i < str.length; i++){
+        if(str.indexOf(i).toUppercase)
+        console.log(strRe);
+    }
+}
+
+capToFront("hApPy"); // APhpy
+capToFront("moveMENT");//MENTmove
+capToFront("shOrtCAKE");//OCAKEshrt
