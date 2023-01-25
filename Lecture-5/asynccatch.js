106c9835fac31b560854cc43409905fc7db1cbbb
thanapatfd <68782076+thanapatfd@users.noreply.github.com>
2023-01-25 15:11:13 +0700

    Create asynccatch.js

---
+++ Lecture-5/asynccatch.js
@@ -0,0 +1,26 @@
+function yayOrNay() {
+    return new Promise((resolve, reject) =>{
+        const val = Math.round(Math.random() * 1);
+        
+        val ? resolve("Lucky!!") : reject("Nope- -*");
+    });
+}
+
+
+async function msg(){
+    try{
+        const msg = await yayOrNay();
+        console.log(msg);
+    } catch(err){
+        console.log(err);
+    }
+}
+
+msg();
+msg();
+msg();
+msg();
+msg();
+msg();
+msg();
+msg();
